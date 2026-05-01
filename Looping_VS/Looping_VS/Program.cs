ac79a69edfc6e37ee167483ea8d7976b15683028
LimbongTiara <12410192@student.unklab.ac.id>
2026-05-01 14:16:24 +0800

    Add files via upload

---
+++ Looping_VS/Looping_VS/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp10
+{     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            n = 10;
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Angka ke {0}", i);
+            }
+            Console.Read();
+        }
+    }
+}
